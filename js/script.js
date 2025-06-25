4c04499646bc95bb3e3a90bee645c550aaeb1da5
Salavat <salavat2602@mail.ru>
2025-06-25 23:04:23 +0500

    modal

--- js/script.js
+++ js/script.js
@@ -106,5 +106,56 @@ window.document.addEventListener("DOMContentLoaded", () => {
         setClock('.timer', deadLine);
 
 
+        //modal
 
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+              modal = document.querySelector('.modal'),
+              modalCloseBtn = document.querySelector('[data-close]');
+
+              modalTrigger.forEach(btn => {
+                btn.addEventListener('click', openModal)
+              })
+
+        
+          
+
+        function openModal () {
+          
+          modal.classList.add('show');
+          modal.classList.remove('hide');
+          document.body.style.overflow = 'hidden';
+          clearInterval(modalTimerId);
+        }
+
+
+        function modalClose() {
+          modal.classList.remove('show');
+          modal.classList.add('hide')
+          document.body.style.overflow = '';
+        }
+
+        modalCloseBtn.addEventListener('click', modalClose)   
+
+        modal.addEventListener('click', (e) => {
+          if (e.target === modal) {
+            modalClose();
+          }
+        });
+
+          document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape" && modal.classList.contains('show')) {
+              modalClose();
+            }
+          });
+
+          const modalTimerId = setTimeout(openModal, 3000);
+
+          function showModalByScroll() {
+             if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+                openModal();
+                window.removeEventListener('scroll', showModalByScroll)
+              }
+          }
+
+          window.addEventListener('scroll', showModalByScroll);
 });
