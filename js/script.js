d3af4fdf00be765fb23ff0ad54eb7fb2218bf8bd
Salavat <salavat2602@mail.ru>
2025-06-26 22:12:34 +0500

    php server

--- js/script.js
+++ js/script.js
@@ -148,7 +148,7 @@ window.document.addEventListener("DOMContentLoaded", () => {
             }
           });
 
-          // const modalTimerId = setTimeout(openModal, 3000);
+          const modalTimerId = setTimeout(openModal, 3000);
 
           function showModalByScroll() {
              if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -243,4 +243,60 @@ window.document.addEventListener("DOMContentLoaded", () => {
         'menu__item',
         'big'
     ).render();
+
+
+    //forms
+
+    const forms =document.querySelectorAll('form');
+
+    const message = {
+      loading : "Загрузка",
+      success : "Спасибо! Скоро мы с вами свяжемся",
+      failure : "Что-то пошло не так..."
+    }
+
+    forms.forEach(item => {
+      postData(item);
+    });
+
+      function postData(form) {
+        form.addEventListener('submit', (e) => {
+          e.preventDefault();
+
+          const statusMessage = document.createElement('div');
+          statusMessage.classList.add('status');
+          statusMessage.textContent = message.loading;
+          form.append(statusMessage);
+
+          const request = new XMLHttpRequest();
+          request.open('POST', 'server.php');
+
+          request.setRequestHeader('Content-type', 'application/json');
+          const formData = new FormData(form);
+
+          const object = {};
+          formData.forEach(function(value, key ){
+            object[key] = value;
+          });
+
+          const json = JSON.stringify(object);
+
+          request.send(json);
+
+          request.addEventListener('load', () => {
+            if (request.status === 200) {
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(() => {
+                  statusMessage.remove();
+                }, 5000);
+                
+            }else {
+              statusMessage.textContent = message.failure;
+            }
+          });
+
+        });
+      }
 });
